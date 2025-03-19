3176c4951fef70be148a14f977b516e696974336
manuelalacerda <manuelalacerda88@gmail.com>
2025-03-19 10:02:43 -0300

    Primeiro programa

---
+++ programamedia.py
@@ -0,0 +1,13 @@
+Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+======= RESTART: C:/Users/User/OneDrive/Área de Trabalho/programamedia.py ======
+---------------------
+*-* Calculo da Média *-*
+---------------------
+N1: 4.7
+N2: 8.9
+N3: 7.5
+Média:  7.033333333333334
+>>> 
+>>> 
